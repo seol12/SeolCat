dc92c481af67a38f63d3da3fb36aa6539a6603e1
Seol12 <seole0408@naver.com>
2022-11-30 17:09:28 +0900

    UPDATE - /pages/_document Attribute 변경

--- src/client/pages/_document.js
+++ src/client/pages/_document.js
@@ -34,9 +34,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang='ko'>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+          <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap' rel='stylesheet' />
         </Head>
         <body>
           <Main />
